e0a7931d72bb77a73512654f57f44d661aba100a
Jerson Zúñiga <jzunigacoayla@gmail.com>
2025-01-28 18:19:11 -0500

    test: padLeft given a string

--- test/main.test.ts
+++ test/main.test.ts
@@ -3,3 +3,7 @@ import { padLeft } from '../src/main';
 test('prepend a number of spaces', () => {
   expect(padLeft(2, 'hola')).toBe('  hola');
 });
+
+test('prepend a string', () => {
+  expect(padLeft('uwu', 'hola')).toBe('uwuhola');
+});
