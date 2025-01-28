c7b9ff9e7e5940211b7242a141b7ef453aae554f
Jerson Zúñiga <jzunigacoayla@gmail.com>
2025-01-28 17:14:00 -0500

    feat: padLeft dummy implementation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+/*
+  If padding is a number, it will treat that as the number of spaces we want
+  to prepend to input. If padding is a string, it should just prepend padding
+  to input.
+ */
+export function padLeft(padding: number | string, input: string): string {
+  return '';
+}
+
 function greet(person: string, date: Date) {
   console.log(`Hello ${person}, today is ${date.toDateString()}!`);
 }
